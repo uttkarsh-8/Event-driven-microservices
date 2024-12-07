f80611e59f2834b827afdf5ef909a8d78c7935e8
uttkarsh-8 <uttkarsh.srivastava12@gmail.com>
2024-12-08 03:07:19 +0530

    first commit

---
+++ src/main/java/com/microservicesd/first/MicroservicesdApplication.java
@@ -0,0 +1,13 @@
+package com.microservicesd.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservicesd/first/MicroservicesdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicesd.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
